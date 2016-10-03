05a4b44540b744b633e7fa8ba8589a2f97c6507c
MBorecki <borecki2@gmail.com>
2016-10-04 01:07:13 +0200

    escape near walls

--- src/ai/orderValidate.js
+++ src/ai/orderValidate.js
@@ -1,9 +1,11 @@
 import {isInDanger, getNextPosition, getPosition} from 'player';
 import {normal, add} from 'utils';
-import {PLAYER_SPEED} from 'config';
+import {PLAYER_SPEED, MAP_W, MAP_H} from 'config';
 
 export default function orderValidate(order) {
     let next = getNextPosition(order);
+
+    printErr('orderValidate', next);
 
     let danger = isInDanger(next);
 
@@ -12,6 +14,7 @@ export default function orderValidate(order) {
     }
 
     if (danger) {
+        // TODO: PLAYER_SPPED -> closest safe place;
         printErr('IN DANGER!', danger);
         let escapePaths = danger.map((enemy) => {
             return normal([next[0] - enemy[0], next[1] - enemy[1]]);
@@ -20,13 +23,38 @@ export default function orderValidate(order) {
         let escapePath = normal(add(escapePaths));
         let pos = getPosition();
 
-        printErr('run to', escapePath);
 
-        // TODO: PLAYER_SPPED -> closest safe place;
+        let x = Math.floor(pos[0] + escapePath[0] * PLAYER_SPEED);
+        let y = Math.floor(pos[1] + escapePath[1] * PLAYER_SPEED);
+
+        printErr('run to #1', x, y);
+
+        if (x < 0) {
+            x = 0;
+            y = ((pos[1] - y) > 0) ? 0 : MAP_H;
+        }
+
+        if (x > MAP_W) {
+            x = MAP_W;
+            y = ((pos[1] - y) > 0) ? 0 : MAP_H;
+        }
+
+        if (y < 0) {
+            x = ((pos[0] - x) > 0) ? 0 : MAP_W;
+            y = 0;
+        }
+
+        if (y > MAP_H) {
+            x = ((pos[0] - x) > 0) ? 0 : MAP_W;
+            y = MAP_H;
+        }
+
+        printErr('run to #2', x, y);
+
         return orderValidate({
             action: 'MOVE',
-            x: Math.floor(pos[0] + escapePath[0] * PLAYER_SPEED),
-            y: Math.floor(pos[1] + escapePath[1] * PLAYER_SPEED)
+            x,
+            y
         })
     } else {
         printErr('SAVE!');
