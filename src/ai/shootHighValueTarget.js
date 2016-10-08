cf7fe09de4521db3e334e73c8279236b84c787e6
MBorecki <borecki2@gmail.com>
2016-10-08 16:28:09 +0200

    reset simulation after Enemy killed

--- src/ai/shootHighValueTarget.js
+++ src/ai/shootHighValueTarget.js
@@ -4,20 +4,24 @@ import {fullSimulation} from 'simulation';
 import optimazeShot from 'ai/optimazeShot';
 
 let targetsCount = null;
+let enemiesCount = null;
 
 export default function() {
 
-    if (targetsCount === null) {
+    if (targetsCount === null || enemiesCount === null) {
         targetsCount = Targets.size;
+        enemiesCount = Enemies.size;
         fullSimulation();
     }
 
-    if (targetsCount !== Targets.size) {
+    if (targetsCount !== Targets.size || enemiesCount !== Enemies.size) {
         fullSimulation();
         targetsCount = Targets.size;
     }
 
     let enemy = Enemies.getHighValueTarget();
+
+    // printErr('4:', Enemies.data.get(4).path);
 
     return optimazeShot(enemy);
 }
